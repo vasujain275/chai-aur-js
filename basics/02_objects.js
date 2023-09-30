3cab2827a5cca0e32ec4c76ae56cf4c44e4dde4a
Vasu Jain <vasujain275@gmail.com>
2023-09-30 17:18:16 +0530

    More Objects Info

--- basics/02_objects.js
+++ basics/02_objects.js
@@ -22,3 +22,30 @@ const JsUser = {
 // console.log(JsUser["name"]);
 // console.log(typeof JsUser[mySym]);
 
+JsUser.greetings = () => {
+    console.log("Hello User!");
+}
+
+JsUser.greetings2 = function () {
+    console.log(`Hello ${this.name}!`);
+}
+
+// JsUser.greetings();
+// JsUser.greetings2();
+
+const obj1 = { 1: 'a', 2: 'b', 3: 'c' };
+const obj2 = { 1: 'd', 2: 'e', 3: 'f' };
+
+const obj3 = { ...obj1, ...obj2, 4: 'g' }
+// console.log(obj3);
+
+console.log(JsUser?.name);
+
+
+const Course = {
+    courseName: 'Chai aur JS',
+    price: 999,
+    courseInstructor: 'Hitesh Choudhary'
+}
+
+const { courseName, price, courseInstructor } = Course; // Destructring Objects
