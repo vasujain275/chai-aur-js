d587aa666878d51f82b94413a657a6c145e07eee
vasujain275 <vasujain275@gmail.com>
2023-12-24 02:08:30 +0530

    Completed till 05_Loops

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,26 +1,22 @@
+// Function Declaration
 const addTwoNumbers = (a, b) => {
     return a + b;
 }
+console.log(addTwoNumbers(1, 2));
 
-// console.log(addTwoNumbers(1, 2));
-
+// Passing default values to function parameters
 function loginUserMsg(username = `Vasu`) {
     return `${username} just logged in`;
 }
-
 console.log(loginUserMsg('Vasu'));
 
 
 // Arrow functions
-
 const addTwoNumbersArrow = (a, b) => {
     return a + b;
 }
-
 console.log(addTwoNumbersArrow(1, 2));
 
 // Arrrow Functions without return keyword and curly braces
-
 const addTwoNumbersArrowDirect = (a, b) => a + b;
-
 console.log(addTwoNumbersArrowDirect(1, 2));
